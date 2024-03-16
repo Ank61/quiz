b7a3f14fd6dc8d521afaa64f0913ba2cdefb95bc
Ankit Chohan <ankitchohan61@gmail.com>
2024-03-16 21:57:03 +0530

    added options with the questions

--- src/pages/pyq/year/[year].tsx
+++ src/pages/pyq/year/[year].tsx
@@ -1,14 +1,29 @@
 import { useRouter } from "next/router";
 import { MainNavbar, MobileResponsiveNav } from "@/components/navbar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
-import  fetchPYQuestions  from "@/services/allAPI";
+import fetchPYQuestions from "@/services/allAPI";
 
 function YearQuestions({ checkYearData }: any) {
   const [questionIndex, setQuestionIndex] = useState<any>(0);
+  const [allQuestions, setAllQuestions] = useState<any>([]);
+  const [allResponse, setAllResponse] = useState<any>([]);
   const router = useRouter();
   const { year } = router.query;
+
+  useEffect(() => {
+    let filteredArray = checkYearData[0]?.questions?.filter(
+      (item: any) => item?.question !== ""
+    );
+    console.log("data", filteredArray);
+    setAllQuestions(filteredArray);
+  }, []);
+
+  useEffect(() => {
+    console.log("Changed Data", allResponse);
+  }, [allResponse]);
+
   const testArray = [
     { question: "Question 1 ssfbksjdbfskjbdfksjbfdjskdbfksjbfdksjbf" },
     { question: "Question 2 ssfbksjdbfskjbdfksjbfdjskdbfksjbfdksjbf" },
@@ -19,6 +34,21 @@ function YearQuestions({ checkYearData }: any) {
     { question: "Question 7 ssfbksjdbfskjbdfksjbfdjskdbfksjbfdksjbf" },
     { question: "Question 8 ssfbksjdbfskjbdfksjbfdjskdbfksjbfdksjbf" },
   ];
+  const handleResponse = (item: any, selectedOption: string) => {
+    debugger;
+    const responseObj = {
+      question: item.question,
+      options: item.select,
+      selectedOption: selectedOption,
+    };
+    //check if the response already exists 
+    allResponse.map((itemQuestion : any)=> {
+      if( item.question === itemQuestion ){
+        //Only push if thew question and the answer 
+        setAllResponse((prev: any) => [...prev,responseObj]);
+      }
+    })
+  };
 
   return (
     <div>
@@ -31,10 +61,34 @@ function YearQuestions({ checkYearData }: any) {
         </span>
         Year Questions
       </div>
-      <div className="mt-36 flex justify-center p-32">
-        {testArray.map((item, index) => {
+      <div className="font-sans font-semibold text-2xl antialiased mt-16 flex justify-center p-4 tracking-wide ">
+        {allQuestions?.map((item: any, index: any) => {
           if (index === questionIndex) {
-            return <div key={index}>{item.question}</div>;
+            return (
+              <div key={index} className="mx-2">
+                <span className="font-bold" style={{ color: "#6e64ff" }}>
+                  Q{index + 1}.
+                </span>{" "}
+                {item.question}
+                <div className="mt-12 tracking-wide">
+                  {item.select.map((options: string, rank: number) => (
+                    <div className="mt-1" key={rank}>
+                      <input
+                        className="mr-4 "
+                        type="radio"
+                        id={options}
+                        name="fav_language"
+                        value={options}
+                        onChange={(e) =>
+                          handleResponse(item, e.target.id)
+                        }
+                      />
+                      <label>{options}</label>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            );
           }
         })}
       </div>
@@ -63,7 +117,6 @@ function YearQuestions({ checkYearData }: any) {
           Next <NavigateNextIcon className="text-purple-700 ml-4 md:ml-4" />
         </div>
       </div>
-      {/* Fetch and display product details based on the productId */}
     </div>
   );
 }
@@ -71,6 +124,8 @@ function YearQuestions({ checkYearData }: any) {
 export default YearQuestions;
 
 export async function getServerSideProps(context: any) {
+  //ServerSide Props meaning each request will get updated API data
+  //Use static props instead
   const { params } = context;
   const fetchYear = params.year;
   const yearData: any = await fetchPYQuestions(fetchYear);
