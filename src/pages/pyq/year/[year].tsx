7c7009b9a911bdd147e9d8bdbf375a8b5ee88f41
Ankit Chohan <ankitchohan61@gmail.com>
2024-03-19 23:32:55 +0530

    year[core] - filtered the response

--- src/pages/pyq/year/[year].tsx
+++ src/pages/pyq/year/[year].tsx
@@ -4,52 +4,53 @@ import { useState, useEffect } from "react";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import fetchPYQuestions from "@/services/allAPI";
-
 function YearQuestions({ checkYearData }: any) {
   const [questionIndex, setQuestionIndex] = useState<any>(0);
   const [allQuestions, setAllQuestions] = useState<any>([]);
   const [allResponse, setAllResponse] = useState<any>([]);
+  const [selectedResponse, setSelectedResponse] = useState('')
+  const [currentIndex, setCurrentIndex] = useState(0)
   const router = useRouter();
   const { year } = router.query;
-
   useEffect(() => {
     let filteredArray = checkYearData[0]?.questions?.filter(
       (item: any) => item?.question !== ""
     );
-    console.log("data", filteredArray);
+    // console.log("data", filteredArray);
     setAllQuestions(filteredArray);
   }, []);
-
   useEffect(() => {
-    console.log("Changed Data", allResponse);
-  }, [allResponse]);
-
-  const testArray = [
-    { question: "Question 1 ssfbksjdbfskjbdfksjbfdjskdbfksjbfdksjbf" },
-    { question: "Question 2 ssfbksjdbfskjbdfksjbfdjskdbfksjbfdksjbf" },
-    { question: "Question 3 ssfbksjdbfskjbdfksjbfdjskdbfksjbfdksjbf" },
-    { question: "Question 4 ssfbksjdbfskjbdfksjbfdjskdbfksjbfdksjbf" },
-    { question: "Question 5 ssfbksjdbfskjbdfksjbfdjskdbfksjbfdksjbf" },
-    { question: "Question 6 ssfbksjdbfskjbdfksjbfdjskdbfksjbfdksjbf" },
-    { question: "Question 7 ssfbksjdbfskjbdfksjbfdjskdbfksjbfdksjbf" },
-    { question: "Question 8 ssfbksjdbfskjbdfksjbfdjskdbfksjbfdksjbf" },
-  ];
-  const handleResponse = (item: any, selectedOption: string) => {
-    debugger;
+    console.log("Initial Data", allQuestions[currentIndex]);
+    console.log("Updated Data", allResponse);
+  }, [allResponse, allQuestions]);
+  const handleResponse = (item: any, selectedOption: string, question: number) => {
     const responseObj = {
+      questionNo: question,
       question: item.question,
       options: item.select,
       selectedOption: selectedOption,
     };
-    //check if the response already exists 
-    allResponse.map((itemQuestion : any)=> {
-      if( item.question === itemQuestion ){
-        //Only push if thew question and the answer 
-        setAllResponse((prev: any) => [...prev,responseObj]);
-      }
-    })
+    const checkDublicate = allResponse.find((item: any) => item.questionNo === responseObj.questionNo);
+    if (checkDublicate) {
+      let updatedPrevious = allResponse;
+      allResponse.forEach((element: any, index: number) => {
+        if (element.questionNo === checkDublicate.questionNo) {
+          updatedPrevious.splice(index, 1);
+          const finalUpdatedResponse = [...updatedPrevious, responseObj].sort((intial: any, next: any) => intial.questionNo - next.questionNo);
+          setAllResponse(finalUpdatedResponse);
+        }
+      });
+    }
+    else {
+      setAllResponse((prev: any) => [...prev, responseObj]);
+    }
+    const findIn = allQuestions.findIndex((ele: any) => ele.question === item.question);
+    if (findIn > -1) {
+      allQuestions[findIn].selectedOption = selectedOption;
+    }
+    setSelectedResponse(selectedOption);
+    setCurrentIndex(question - 1);
   };
-
   return (
     <div>
       <MobileResponsiveNav />
@@ -61,7 +62,7 @@ function YearQuestions({ checkYearData }: any) {
         </span>
         Year Questions
       </div>
-      <div className="font-sans font-semibold text-2xl antialiased mt-16 flex justify-center p-4 tracking-wide ">
+      <div className="font-sans font-semibold text-xl antialiased mt-16 flex justify-center p-3  ">
         {allQuestions?.map((item: any, index: any) => {
           if (index === questionIndex) {
             return (
@@ -70,20 +71,23 @@ function YearQuestions({ checkYearData }: any) {
                   Q{index + 1}.
                 </span>{" "}
                 {item.question}
-                <div className="mt-12 tracking-wide">
+                <div className="mt-12 text-xl">
                   {item.select.map((options: string, rank: number) => (
-                    <div className="mt-1" key={rank}>
+                    <div className="mt-1" id={options} key={options}>
                       <input
                         className="mr-4 "
                         type="radio"
+                        checked={item.selectedOption === options}
                         id={options}
                         name="fav_language"
                         value={options}
                         onChange={(e) =>
-                          handleResponse(item, e.target.id)
+                          handleResponse(item, e.target.id, index + 1)
                         }
                       />
-                      <label>{options}</label>
+                      <label id={options} onClick={(e: any) =>
+                        handleResponse(item, e.target.id, index + 1)
+                      }>{options}</label>
                     </div>
                   ))}
                 </div>
@@ -93,9 +97,8 @@ function YearQuestions({ checkYearData }: any) {
         })}
       </div>
       <div
-        className={`grid ${
-          questionIndex === 0 ? `grid-cols-1` : `grid-cols-2`
-        } gap-2 mt-4 w-full md:w-3/6 md:h-4/6 md:mx-auto my-2 md:my-12`}
+        className={` grid ${questionIndex === 0 ? `grid-cols-1` : `grid-cols-2`
+          } gap-2 mt-4 w-full md:w-3/6 md:h-4/6 md:mx-auto my-2 md:my-12`}
       >
         {questionIndex !== 0 ? (
           <div
@@ -104,15 +107,15 @@ function YearQuestions({ checkYearData }: any) {
                 setQuestionIndex(questionIndex - 1);
               }
             }}
-            className="serviceButton bg-gradient-to-r from-blue-50 to-purple-50 p-4 m-1.5 flex font-sans justify-center rounded-xl items-center text-xl"
+            className={`serviceButton bg-gradient-to-r from-blue-50 to-purple-50 p-4 m-1.5 flex font-sans justify-center rounded-xl items-center text-xl`}
           >
             <ArrowBackIosIcon className="text-purple-700 text-lg mr-4 md:mr-4" />
             Previous
           </div>
         ) : null}
         <div
-          onClick={() => setQuestionIndex(questionIndex + 1)}
-          className="serviceButton bg-gradient-to-r from-blue-50 to-purple-50 font-sans p-4 m-1.5 flex justify-center rounded-xl items-center text-xl"
+          onClick={() => { setQuestionIndex(questionIndex + 1); setSelectedResponse('') }}
+          className={` serviceButton bg-gradient-to-r from-blue-50 to-purple-50 font-sans p-4 m-1.5 flex justify-center rounded-xl items-center text-xl`}
         >
           Next <NavigateNextIcon className="text-purple-700 ml-4 md:ml-4" />
         </div>
@@ -120,9 +123,7 @@ function YearQuestions({ checkYearData }: any) {
     </div>
   );
 }
-
 export default YearQuestions;
-
 export async function getServerSideProps(context: any) {
   //ServerSide Props meaning each request will get updated API data
   //Use static props instead
